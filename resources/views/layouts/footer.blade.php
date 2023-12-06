51a9ef855c16d3815b4c9ce7295c0918da93b55b
sav <aevs.andres.vidal@gmail.com>
2023-12-05 18:02:13 -0600

    fix (web): add networks links on footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,17 +6,17 @@
             </div>
             <ul class="nav col-md-6 ms-auto justify-content-center list-unstyled p-2">
                 <li class="ms-3">
-                    <a class="text-muted" href="">
+                    <a class="text-muted" href="https://app.edutin.com/in/6812540" target="_blank">
                         <img src="{{ asset('build/assets/icon/edutin_academy.png') }}" alt="edutin-academy" height="32">
                     </a>
                 </li>
                 <li class="ms-3">
-                    <a class="text-muted" href="">
+                    <a class="text-muted" href="https://github.com/xxxSAV" target="_blank">
                         <img src="{{ asset('build/assets/icon/github.png') }}" alt="github" height="32">
                     </a>
                 </li>
                 <li class="ms-3">
-                    <a class="text-muted" href="">
+                    <a class="text-muted" href="https://www.linkedin.com/in/samoel-andres-vidal-80733b262/" target="_blank">
                         <img src="{{ asset('build/assets/icon/linkedin.png') }}" alt="linkedin" height="32">
                     </a>
                 </li>
