85684a5006971a344b1c9567851c9b108965a007
sav <aevs.andres.vidal@gmail.com>
2023-12-15 21:08:29 -0600

    refactor (web): change word 'previous' to 'prev' on navigation bar of projects

--- resources/views/projects/pages/blog.blade.php
+++ resources/views/projects/pages/blog.blade.php
@@ -20,7 +20,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#006064" viewBox="0 0 16 16">
                         <path d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/>
                     </svg>
-                    <span>Previous</span>
+                    <span>Prev</span>
                 </a>
             </div>
             <div class="col text-center">
