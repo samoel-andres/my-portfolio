7b82cd9445e1f00769952f90aecd3b3bdbebc977
sav <aevs.andres.vidal@gmail.com>
2023-12-19 19:56:38 -0600

    feat (backend): enabled production mode to send emails from contact page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Mail\NotificationMailable;
+use App\Notifications\MailNotification;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
@@ -31,10 +34,20 @@ class ContactController extends Controller
             'msg' => $request->message,
         ]);
 
-        Mail::to('aevs.andres.vidal@gmail.com')
-            ->send(new NotificationMailable($contact));
+        // Development mode (mailtrap)
+        // Mail::to('aevs.andres.vidal@gmail.com')
+        //     ->send(new NotificationMailable($contact));
+
+        try {
+            // Production mode (sendgrid)
+            Notification::route('mail', 'aevs.andres.vidal@gmail.com')
+                ->notify(new MailNotification($contact));
+        } catch (\Exception $e) {
+            return redirect()->route('contact.index')
+                ->with('send-error', 'Oops, an error ocurred while sending the notification, could you let me know in the contacts below?.');
+        }
 
         return redirect()->route('contact.index')
-            ->with('success-sends', 'Sent correctly');
+            ->with('success-sends', 'Sent correctly, I will respond to you shortly or contact you at phone number you provided me.');
     }
 }
