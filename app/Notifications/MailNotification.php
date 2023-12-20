9fa487ac3ac106b05a043fe548e98be280c4b71f
sav <aevs.andres.vidal@gmail.com>
2023-12-19 20:02:13 -0600

    feat (backend): MailNotification is used to send notification to me, collect the data provided by user interested on me

---
+++ app/Notifications/MailNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class MailNotification extends Notification
+{
+    use Queueable;
+
+    public $contact;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->replyTo($this->contact['email'], $this->contact['name'])
+            ->subject('Someone is interested in you')
+            ->view('contact.notification', ['contact' => $this->contact]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
