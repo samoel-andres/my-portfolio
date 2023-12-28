49b9a15f9baff200613e2e1e02f5d5c7164e07ee
sav <aevs.andres.vidal@gmail.com>
2023-12-28 02:12:23 -0600

    other (web): fix a word (doesn´t affect the user)

--- resources/views/projects/pages/citasweb.blade.php
+++ resources/views/projects/pages/citasweb.blade.php
@@ -119,7 +119,7 @@
                                 <path d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/>
                             </svg>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#high-fidelity-carousel" data-bs-slide="next" name="next-high-felidity">
+                        <button class="carousel-control-next" type="button" data-bs-target="#high-fidelity-carousel" data-bs-slide="next" name="next-high-fidelity">
                             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#006064" viewBox="0 0 16 16">
                                 <path d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/>
                             </svg>
