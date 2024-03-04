3daa0a68fdd7e8fd8f56234b9b709f8568e5e2bc
sav <aevs.andres.vidal@gmail.com>
2024-03-04 16:17:41 -0600

    other (web): date of copyright updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container border-top">
         <div class="row align-items-center">
             <div class="col-md-6 text-center">
-                <span> &copy; Samoel Andres 2023 | Web developer</span>
+                <span>&copy; Samoel Andres 2024 | Web developer</span>
             </div>
             <ul class="nav col-md-6 ms-auto justify-content-center list-unstyled p-2">
                 <li class="ms-3">
@@ -16,7 +16,7 @@
                     </a>
                 </li>
                 <li class="ms-3">
-                    <a class="text-muted" href="https://www.linkedin.com/in/samoel-andres-vidal-80733b262/" target="_blank">
+                    <a class="text-muted" href="https://www.linkedin.com/in/samoel-andres-vidal" target="_blank">
                         <img src="{{ asset('build/assets/icon/linkedin.webp') }}" alt="linkedin" height="32" width="32">
                     </a>
                 </li>
