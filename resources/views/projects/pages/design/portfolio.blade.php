c76fac799737337cfdefc5cfb1ceaaa4590f0eec
sav <aevs.andres.vidal@gmail.com>
2024-03-06 16:47:44 -0600

    fix (intern): route correction in href

--- resources/views/projects/pages/design/portfolio.blade.php
+++ resources/views/projects/pages/design/portfolio.blade.php
@@ -122,7 +122,7 @@
                 </a>
             </div>
             <div class="col text-end">
-                <a class="btn" href="{{ route('projects.blog') }}">
+                <a class="btn" href="{{ route('projects.designonstore') }}">
                     <span>Next</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#006064" viewBox="0 0 16 16">
                         <path d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/>
