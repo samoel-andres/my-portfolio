6d0aefce65d152bd69f58ef3ea36ca4cc7234bd8
sav <aevs.andres.vidal@gmail.com>
2024-09-29 17:26:06 -0600

    spanish traslation

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="{{ asset('build/assets/css/about/about.css') }}">
 @endsection
 
-@section('title', 'About - Samoel Andres')
+@section('title', 'Sobre mí - Samoel Andres')
 
 @section('content')
 <div class="container my-4 fade-in">
@@ -15,17 +15,19 @@
             <div class="col">
                 <h1 class="floating-5" style="box-shadow: none;">SAMOEL ANDRES</h1>        
                 <p class="floating-5" style="box-shadow: none; color: #535353;">
-                    Samoel is a programmer focused on web development (recently<br>
-                    graduated) -- currently he looking job.
+                    Samoel es un apasionado del desarrollo web, programación y le
+                    encanta combinar el desarrollo con la experiencia de usuario.
                 </p>
                 <p class="floating-5" style="box-shadow: none; color: #535353;">
-                    He has worked on school projects and some private companies.<br>
-                    His most recent archievement has been the development of an<br>
-                    appointment system, this developed in school residences<br>
-                    for a private company.
+                    Siempre esta en busca de nuevos desafíos que le permitan
+                    continuar su crecimiento y le ayuden a mejorar. Ha trabajado en
+                    proyectos escolares e instituciones privadas.
+                    <br><br>
+                    <b>Su logro más reciente:</b> Desarrollo de un sistema de citas
+                    para una entidad privada.
                 </p>
                 <a class="btn btn-animate floating-3" href="{{ route('projects.index') }}">
-                    <span>My projects</span>
+                    <span>Mis proyectos</span>
                     <svg width="13px" height="10px" viewBox="0 0 13 10">
                         <path d="M1,5 L11,5"></path>
                         <polyline points="8 1 12 5 8 9"></polyline>
@@ -36,74 +38,81 @@
         <div class="col-md-6 top-2">
             <div class="row text-center">
                 <div class="col">
-                    <img src="{{ asset('build/assets/image/photo.webp') }}" class="img-fluid floating-3" style="border-radius: 500px;" alt="photo-samoel-andres">
+                    <img src="{{ asset('build/assets/image/dev.gif') }}" class="img-fluid floating-3" style="border-radius: 500px;" alt="picture-gif">
                 </div>
             </div>
         </div>
     </div>
     <div class="row justify-content-between m-top-5 bg-white floating-5 p-top-05 border">
         <div class="col-md-2 floating-5" style="box-shadow: none;">
-            <h5>Education</h5>                
+            <h5>Educación</h5>                
             <p style="color: #535353; font-size: 12px;">
                 Instituto Tecnológico, Cuautla, Morelos<br>
-                Systems engineer<br>
+                Ingeniería en Sistemas<br>
                 2019 - 2023
                 <br><br>
                 CU Benjamin Franklin, Cuautla, Morelos<br>
-                Computer technical<br>
+                Técnico en computación<br>
                 2015 - 2018
             </p>
         </div>
         <div class="col-md-2 floating-4" style="box-shadow: none;">
-            <h5>Experience</h5>
+            <h5>Experiencia</h5>
             <p style="color: #535353; font-size: 12px;">
-                Private<br>
-                Developer intern<br>
-                March - September, 2023
+                Telmex<br>
+                Pasante de desarrollo<br>
+                Marzo - Septiembre, 2023
                 <br><br>
-                Private<br>
-                Full stack developer<br>
-                March - April, 2021
+                BBVA<br>
+                Participante de hackathon<br>
+                Octubre, 2022
+                <br><br>
+                Privada<br>
+                Desarrollador full stack<br>
+                Marzo - Abril, 2021
             </p>
         </div>
         <div class="col-md-2 floating-3" style="box-shadow: none;">
-            <h5>Skills</h5>
+            <h5>Habilidades</h5>
             <p style="color: #535353; font-size: 12px;">
-                Analysis<br>
-                UX/UI Design<br>
-                Web development<br>
+                Analizar<br>
+                Organizar<br>
+                Colaborar<br>
+                Comunicar<br>
+                Innovar e idear<br>
+                Resolver<br>
+                Adaptar<br>
+                Alcanzar
             </p>
         </div>
         <div class="col-md-2 floating-4" style="box-shadow: none;">
-            <h5>Technical skills</h5>
+            <h5>Habilidades técnicas</h5>
             <p style="color: #535353; font-size: 12px;">
-                JavaScript<br>
-                MongoDB<br>
-                MySQL<br>
+                Java | JavaScript<br>
+                MongoDB | MySQL<br>
                 Node.js<br>
                 Express.js<br>
                 Angular<br>
                 Bootstrap<br>
                 CSS3<br>
                 HTML5<br>
-                Git/GitHub<br>
-                Laravel<br>
-                PHP<br>
-                Adobe XD<br>
-                Figma<br>
-                Frontend<br>
-                Backend
+                Git | GitHub<br>
+                PHP | Laravel<br>
+                Adobe XD | Figma<br>
+                Diseño UX/UI<br>
+                Front end | Back end<br>
             </p>
         </div>
         <div class="col-md-2 floating-5" style="box-shadow: none;">
-            <h5>Courses & certifications</h5>
+            <h5>Cursos & certificaciones</h5>
             <p style="color: #535353; font-size: 12px;">
                 Laravel<br>
                 Wordpress<br>
                 Node.js<br>
-                User Experience Design (UX/UI)<br>
-                Structure and Management database with MySQL<br>
-                PHP
+                Diseño de experiencia de usuario (UX/UI)<br>
+                Estructura y manejo de BD con MySQL<br>
+                Curso de PHP<br>
+                Introducción a Java
             </p>
         </div>
     </div>
