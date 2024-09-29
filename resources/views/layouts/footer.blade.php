499701980f0112a0a28bd2e9441747f0ac988dd4
sav <aevs.andres.vidal@gmail.com>
2024-09-29 17:22:37 -0600

    spanish traslation

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container border-top">
         <div class="row align-items-center">
             <div class="col-md-6 text-center">
-                <span>&copy; Samoel Andres 2024 | Web developer</span>
+                <span>&copy; 2024 Samoel Andres | Desarrollo | UX/UI</span>
             </div>
             <ul class="nav col-md-6 ms-auto justify-content-center list-unstyled p-2">
                 <li class="ms-3">
@@ -11,7 +11,7 @@
                     </a>
                 </li>
                 <li class="ms-3">
-                    <a class="text-muted" href="https://github.com/xxxSAV" target="_blank">
+                    <a class="text-muted" href="https://github.com/samoel-andres" target="_blank">
                         <img src="{{ asset('build/assets/icon/github.webp') }}" alt="github" height="32" width="32">
                     </a>
                 </li>
