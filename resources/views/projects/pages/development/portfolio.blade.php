243ae57ddd967f394aca4f12a918efce66815b41
sav <aevs.andres.vidal@gmail.com>
2024-09-29 17:23:27 -0600

    spanish traslation

--- resources/views/projects/pages/development/portfolio.blade.php
+++ resources/views/projects/pages/development/portfolio.blade.php
@@ -7,102 +7,40 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
             </svg>
-            <span>Close details</span>
+            <span>Cerrar detalles</span>
         </a>
     </div>
 </div>
 <div class="row py-2 fade-in">
     <main>
-        <h3>My portfolio web</h3>
-        <span>Frontend (Laravel with PHP 8.1)</span>
+        <h3>Mi portafolio (Web)</h3>
+        <span>Front end (Laravel con PHP 8.1)</span>
         <br><br><br>
-        <h5>The challenge</h5>
+        <h5>El reto</h5>
         <p class="text-start" style="color: #535353;">
-            Develop a personal web to display information about of me (short information personal,
-            work history/projects done and education). This to endorse my work experience and with
-            curricular porpouses.
+            Desarrollar las interfaces diseñadas para mi propio portafolio web, en el debo mostrar
+            información breve, historial laboral, proyectos realizados y educación.
         </p>
-        <h5>Process</h5>
+        <h5>Proceso</h5>
         <ol class="list-group list-group-numbered">
             <li class="list-group-item bg-white" style="color: #006064;">
-                Design of views (low fidelity)
+                Realizar las vistas
                 <p class="text-start ms-3" style="color: #535353;">
-                    Having clear the objetive of the development, I started with ideas for the site 
-                    views.
+                    Laravel es un framework de PHP que funciona por medio de un sistema de paquetes usando
+                    la arquitectura modelo-vista-controlador.
+                </p>
+                <p class="text-start ms-3" style="color: #535353;">
+                    Todas las vistas de mi proyecto se renderizan dinámicamente gracias a que el framework
+                    funciona con el motor de plantillas Blade, esto permite optimizar la lógica de cada vista
+                    evitando tener que escribir repetidamente la misma sintaxis de marcado (HTML).
                 </p>
                 <div class="col-md-10 ms-3">
-                    <div id="low-fidelity-carousel" class="carousel slide border" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active text-center" data-bs-interval="9000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/low_fidelity_7.webp') }}" alt="design-low-fidelity-7">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/low_fidelity_8.webp') }}" alt="design-low-fidelity-8">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/low_fidelity_9.webp') }}" alt="design-low-fidelity-9">
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#low-fidelity-carousel" data-bs-slide="prev" name="prev-low-fidelity" id="prev-low-fidelity">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#006064" viewBox="0 0 16 16">
-                                <path d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/>
-                            </svg>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#low-fidelity-carousel" data-bs-slide="next" name="next-low-fidelity" id="next-low-fidelity">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#006064" viewBox="0 0 16 16">
-                                <path d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/>
-                            </svg>
-                        </button>
-                    </div>
+                    <img class="img-fluid border" src="{{ asset('build/assets/image/other/interfaces_portfolio.webp') }}" alt="components-angular">
                 </div>
                 <br>
-            </li>
-            <li class="list-group-item bg-white" style="color: #006064;">
-                Design of views (high fidelity)
                 <p class="text-start ms-3" style="color: #535353;">
-                    In this step, I realized those ideas in high fidelity before laying then out.
-                </p>
-                <div class="col-md-10 ms-3">
-                    <div id="high-fidelity-carousel" class="carousel slide border" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active text-center" data-bs-interval="9500">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/high_fidelity_11.webp') }}" alt="design-high-fidelity-11">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/high_fidelity_12.webp') }}" alt="design-high-fidelity-12">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/high_fidelity_12_1.webp') }}" alt="design-high-fidelity-12-1">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/high_fidelity_13.webp') }}" alt="design-high-fidelity-13">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/high_fidelity_13_1.webp') }}" alt="design-high-fidelity-13-1">
-                            </div>
-                            <div class="carousel-item text-center" data-bs-interval="2000">
-                                <img class="d-block w-100" src="{{ asset('build/assets/image/other/high_fidelity_14.webp') }}" alt="design-high-fidelity-14">
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#high-fidelity-carousel" data-bs-slide="prev" name="prev-high-fidelity" id="prev-high-fidelity">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#006064" viewBox="0 0 16 16">
-                                <path d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/>
-                            </svg>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#high-fidelity-carousel" data-bs-slide="next" name="next-high-fidelity" id="next-high-fidelity">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#006064" viewBox="0 0 16 16">
-                                <path d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <br>
-            </li>
-            <li class="list-group-item bg-white" style="color: #006064;">
-                Make views
-                <p class="text-start ms-3" style="color: #535353;">
-                    The views are rendered dynamically, are extensions of a base to avoid
-                    writing code repeatedly.
+                    A continuación, puedes visualizar una breve galeria de evidencias del desarrollo, solo
+                    se visualiza contenido de algunos archivos ya que son bastantes y extensos.
                 </p>
                 <div class="col-md-10 ms-3">
                     <div id="evidence-carousel" class="carousel slide border" data-bs-ride="carousel">
@@ -129,21 +67,23 @@
                 <br>
             </li>
             <li class="list-group-item bg-white" style="color: #006064;">
-                Results
+                Resultados
                 <p class="text-start ms-3" style="color: #535353;">
-                    The image is illustrative, the results is this site that you are viewing.
+                    La imagen es ilustrativa, los resultados de este proyecto los estas experimentando en tiempo
+                    real, es una web responsiva que se adapta a el tamaño de pantalla de los dispositivos desde
+                    los que se visualiza.
                 </p>
                 <div class="col-md-10 ms-3">
-                    <img class="img-fluid border" src="{{ asset('build/assets/image/other/result_devices_1.webp') }}" alt="result-devices">
+                    <img class="img-fluid border" src="{{ asset('build/assets/image/other/devices_web_portfolio.webp') }}" alt="result-devices">
                 </div>
                 <br>
             </li>
         </ol>
-        <h5 style="color: #006064;">Note</h5>
+        <h5 style="color: #006064;">Nota</h5>
         <p class="text-start" style="color: #535353;">
-            This is a personal project, I hope you like it and it is of interest to you, if you wish you can contact me
-            through <a style="color: #006064;" href="{{ route('contact.index') }}">my contact page</a>. You can view the
-            code of this project on my <a style="color: #006064;" href="https://github.com/xxxSAV" target="_blank">GitHub</a>, 
+            Este es un proyecto personal, espero que sea de tu agrado e interés, si lo deseas puedes contactar conmigo a través de
+            <a style="color: #006064;" href="{{ route('contact.index') }}">mi página de contacto</a>. Puedes ver el codigo de este
+            proyecto en mi <a style="color: #006064;" href="https://github.com/samoel-andres" target="_blank">GitHub</a>, 
         </p>
         <div class="col-md-10">
             <img class="img-fluid border" src="{{ asset('build/assets/image/other/evidence_22.webp') }}" alt="evidence-22">
@@ -155,17 +95,17 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#006064" viewBox="0 0 16 16">
                         <path d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/>
                     </svg>
-                    <span>Prev</span>
+                    <span>Ant</span>
                 </a>
             </div>
             <div class="col text-center">
                 <a href="#" id="up" class="btn">
-                    <span>Slide up</span>
+                    <span>Subir</span>
                 </a>
             </div>
             <div class="col text-end">
                 <a class="btn" href="{{ route('projects.blog') }}">
-                    <span>Next</span>
+                    <span>Sig</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#006064" viewBox="0 0 16 16">
                         <path d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/>
                     </svg>
